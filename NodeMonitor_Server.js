5f5ff08453fa50e132bcbbb64e4945a4734541ef
meltingice <meltingice8917@gmail.com>
2010-09-01 11:35:11 -0400

    Added HTTP server with WebSockets provided by Socket.IO. Currently, HTTP server outputs a simple hello world for proof-of-concept.

--- NodeMonitor_Server.js
+++ NodeMonitor_Server.js
@@ -8,17 +8,23 @@
  * Source code is hosted at http://github.com/meltingice/NodeMonitor
  */
 
-var net = require('net');
+var net 	= require('net'),
+	http	= require('http'),
+	io		= require('./lib/socket.io'),
+	fs		= require('fs');
 
 var MonitorServer = {
 	config: require('./config/server_config'),
-	server: false
+	server: false,
+	httpserver: null,
+	websocket: null
 };
 
 MonitorServer.start = function() {
 	console.log('Starting NodeMonitor Server!');
 	
 	this.start_node_listener();
+	this.start_web_server();
 }
 
 MonitorServer.start_node_listener = function() {
@@ -44,6 +50,38 @@ MonitorServer.start_node_listener = function() {
 	
 	this.server.listen(this.config.node_listen_port, this.config.node_listen_addr);
 	console.log("NodeMonitor Server now listening on " + this.config.node_listen_addr + ":" + this.config.node_listen_port);
+}
+
+MonitorServer.start_web_server = function() {
+	this.httpserver = http.createServer(function(req, res) {
+		res.writeHead(200, {});
+		
+		var url;
+		if(req.url == '/') { url = '/index.html'; }
+		else {
+			// security measure
+			url = req.url.replace('..', '');
+		}
+		
+		console.log(req.socket.remoteAddress + ' - ' + req.method + ' ' + url);
+		fs.readFile('./html' + url, function(err, data) {
+			if(err) {
+				res.close();
+				return true;
+			}
+			
+			res.write(data);
+			res.close();
+		});
+	});
+	
+	this.httpserver.listen(this.config.http_listen_port);
+	
+	this.websocket = io.listen(this.httpserver);
+	this.websocket.on('connection', function(client) {
+		console.log("Web client connected!");
+		console.log(client);
+	});
 }
 
 // Start the server!
--- config/server_config.js
+++ config/server_config.js
@@ -3,3 +3,6 @@ this.node_listen_addr = '127.0.0.1';
 
 // The port to use for incoming connections from nodes
 this.node_listen_port = 88;
+
+// The port to serve the webpage over
+this.http_listen_port = 8080;
