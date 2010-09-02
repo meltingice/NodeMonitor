e71bf3d165dbb86bbb3ad368f937e06df03b1d30
meltingice <meltingice8917@gmail.com>
2010-09-02 16:21:09 -0400

    Monitoring webpage now actually begins to render content.  Only placeholders for now.

--- html/js/nodemonitor.js
+++ html/js/nodemonitor.js
@@ -7,6 +7,8 @@ var NM = window.NM || {};
 		window.console.log = function(data){ }
 	}
 	
+	NM.nodes = {};
+	
 	NM.init = function() {
 		io.setPath('/js/socket_io/');
 		
@@ -55,9 +57,76 @@ var NM = window.NM || {};
 	}
 	
 	NM.parse_data = function(data) {
-		data = JSON.parse(data);
-		console.log(data);
+		try{
+			
+			data = JSON.parse(data);
+			if(!this.nodes[data.origin]) {
+				this.add_node(data.origin);
+			}
+			
+			if(!this.nodes[data.origin].plugins[data.plugin]) {
+				this.add_plugin(data.origin, data.plugin, data.render);
+			}
+			
+			this.nodes[data.origin].plugins[data.plugin].data = data.data;
+			this.render_data(data);
+			
+		} catch(err){ }
+	}
+	
+	NM.add_node = function(name) {
+		if(!this.nodes[name]) {
+			this.nodes[name] = {
+				name: name,
+				plugins: {}
+			};
+			
+			NMR.add_node(name);
+		}
+	}
+	
+	NM.add_plugin = function(node, name, render) {
+		this.nodes[node].plugins[name] = {
+			render: render,
+			data: null
+		};
 		
+		NMR.add_plugin(node, name);
+	}
+	
+	// The rendering class
+	var NMR = {};
+	
+	NMR.name_to_css = function(name) {
+		return name.replace(/([^A-Za-z0-9_-]+)/g, '');
+	}
+	
+	NMR.add_node = function(name) {
+		var div_name = 'node-'+this.name_to_css(name);
+		$('<div />')
+			.attr('id', div_name)
+			.html('<h1>'+name+'</h1>\n<div class="node-content"></div>\n')
+			.appendTo('#nodes');
+	}
+	
+	NMR.add_plugin = function(node, name) {
+		var plugin = NM.nodes[node].plugins[name];
+		switch(plugin.render.type) {
+			case 'text': return this.add_text_plugin(node, name);
+			case 'table': return this.add_table_plugin(node, name);
+		}
+	}
+	
+	NMR.add_text_plugin = function(node, name) {
+		var plugin = NM.nodes[node].plugins[name];
+		
+		var parent = 'node-'+this.name_to_css(node);
+		var class = 'plugin-'+this.name_to_css(name);
+		$('<div />')
+			.addClass(class)
+			.addClass('text-plugin')
+			.html('<h1>'+plugin.render.title+'</h1><div class="plugin-content"></div>')
+			.appendTo('#'+parent+'> .node-content');
 	}
 	
 })(window);
